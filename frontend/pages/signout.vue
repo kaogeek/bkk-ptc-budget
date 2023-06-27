74ba48ae6ad955519105e0a531773beaeacce7b3
bovornsiriampairat <bovorn.siriampairat@gmail.com>
2023-06-27 11:17:37 +0700

    Updated

---
+++ frontend/pages/signout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <NavBar/>
+    </div>
+</template>
+<script>
+export default {
+  mounted() {
+    if (process.client) {
+      window.sessionStorage.clear();
+      this.$router.push('/')
+    }
+  },
+};
+</script>
