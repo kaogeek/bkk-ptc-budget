f9bfeb48587bb0eff73e19e566fa9c2ebc7b277d
bovornsiriampairat <bovorn.siriampairat@gmail.com>
2023-06-27 13:17:05 +0700

    Updated

---
+++ frontend/pages/admin/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div class="container">
+            Management System
+        </div>
+    </div>
+</template>
---
+++ frontend/pages/admin/user/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div class="container">
+            User Management System
+        </div>
+    </div>
+</template>
