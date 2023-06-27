55b920d34864401d55482618068cb3ec7d0d99b2
bovornsiriampairat <bovorn.siriampairat@gmail.com>
2023-06-27 11:23:52 +0700

    Fixed Bug

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -39,9 +39,6 @@
                 </NuxtLink>
             </div>
             <div class="col-md-3 text-end" v-else>
-                <button type="button" class="btn btn-outline-primary me-2"
-                    style="background:#EF4D4E;;border-radius: 4px;color:#FFFFFF;border-color:#EF4D4E;font-size:14px;">เพิ่มโครงการ</button>
-
                 <NuxtLink to="signin">
                     <button type="button" class="btn btn-outline-secondary me-2"
                         style="border:none;color:#606060;border-color:#EF4D4E;font-size:14px;">เข้าสู่ระบบ</button>
