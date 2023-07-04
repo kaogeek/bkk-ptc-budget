e3a155acdcd757b0240883b68214d845c073bde0
bovornsiriampairat <bovorn.siriampairat@gmail.com>
2023-07-04 12:40:51 +0700

    Updated

---
+++ frontend/pages/forgot-password.vue
@@ -0,0 +1,17 @@
+<template>
+    <Head>
+        <Title>ชุมชนโปร่งใส - ลืมรหัสผ่าน</Title>
+    </Head>
+    <NavBar />
+    <div class="container" style="margin-top:94px;">
+        <p style="font-size:36px;font-weight:100;" class="text-center">ลืมรหัสผ่าน</p>
+        <div class="row">
+            <section class="h-100 h-custom">
+                <div class="container py-5 h-100">
+                  
+                </div>
+            </section>
+        </div>
+    </div>
+    <Footer/>
+</template>
